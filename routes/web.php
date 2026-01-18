91235e380f4c213d153d0f9e1a5231bee476a5c5
FrdAkbr <frdakbr9@gmail.com>
2026-01-19 05:16:01 +0800

    deploy ulang

--- routes/web.php
+++ routes/web.php
@@ -6,15 +6,45 @@ use App\Http\Controllers\Admin\RestaurantAdminController;
 use App\Http\Controllers\Admin\AuthController;
 use App\Http\Controllers\UserController;
 
-Route::post('/users', [UserController::class, 'store'])->name('users.store');
+/*
+|--------------------------------------------------------------------------
+| FRONTEND ROUTES - HALAMAN PUBLIK
+|--------------------------------------------------------------------------
+*/
+Route::get('/', [RestaurantController::class, 'index'])
+    ->name('home');
+
+Route::get('/semua', [RestaurantController::class, 'semua'])
+    ->name('restaurant.semua');
+
+Route::get('/restaurant/{restaurant}', [RestaurantController::class, 'show'])
+    ->name('restaurant.show');
+
+Route::get('/search', [RestaurantController::class, 'search'])
+    ->name('restaurant.search');
+
+Route::post('/review/{id}', [RestaurantController::class, 'storeReview'])
+    ->name('review.store');
 
 /*
 |--------------------------------------------------------------------------
-| AUTH ADMIN
+| USER ROUTES
+|--------------------------------------------------------------------------
+*/
+Route::get('/users/create', function () {
+    return view('users.create');
+})->name('users.create');
+
+Route::post('/users', [UserController::class, 'store'])
+    ->name('users.store');
+
+/*
+|--------------------------------------------------------------------------
+| ADMIN AUTHENTICATION ROUTES
 |--------------------------------------------------------------------------
 */
 Route::get('/admin/login', [AuthController::class, 'loginForm'])
-    ->name('login'); // ⬅️ PENTING
+    ->name('login');
 
 Route::post('/admin/login', [AuthController::class, 'login'])
     ->name('admin.login.submit');
@@ -24,33 +54,25 @@ Route::post('/admin/logout', [AuthController::class, 'logout'])
 
 /*
 |--------------------------------------------------------------------------
-| ADMIN RESTAURANT (TANPA AUTH DULU BIAR AMAN)
+| ADMIN RESTAURANT MANAGEMENT ROUTES
 |--------------------------------------------------------------------------
 */
 Route::prefix('admin')->group(function () {
+    Route::get('/restaurants', [RestaurantAdminController::class, 'index'])
+        ->name('admin.restaurants.index');
 
-    Route::get('/restaurants', [RestaurantAdminController::class, 'index']);
-    Route::get('/restaurants/create', [RestaurantAdminController::class, 'create']);
-    Route::post('/restaurants', [RestaurantAdminController::class, 'store']);
-    Route::get('/restaurants/{id}/edit', [RestaurantAdminController::class, 'edit']);
-    Route::put('/restaurants/{id}', [RestaurantAdminController::class, 'update']);
-    Route::delete('/restaurants/{id}', [RestaurantAdminController::class, 'destroy']);
+    Route::get('/restaurants/create', [RestaurantAdminController::class, 'create'])
+        ->name('admin.restaurants.create');
 
+    Route::post('/restaurants', [RestaurantAdminController::class, 'store'])
+        ->name('admin.restaurants.store');
+
+    Route::get('/restaurants/{id}/edit', [RestaurantAdminController::class, 'edit'])
+        ->name('admin.restaurants.edit');
+
+    Route::put('/restaurants/{id}', [RestaurantAdminController::class, 'update'])
+        ->name('admin.restaurants.update');
+
+    Route::delete('/restaurants/{id}', [RestaurantAdminController::class, 'destroy'])
+        ->name('admin.restaurants.destroy');
 });
-
-/*
-|--------------------------------------------------------------------------
-| FRONTEND
-|--------------------------------------------------------------------------
-*/
-Route::get('/', [RestaurantController::class,'index']);
-Route::get('/restaurant/{restaurant}', [RestaurantController::class,'show'])
-    ->name('restaurant.show');
-
-Route::post('/review/{id}', [RestaurantController::class,'storeReview']);
-
-Route::get('/semua', [RestaurantController::class, 'semua'])
-    ->name('restaurant.semua');
-
-Route::get('/search', [RestaurantController::class, 'search'])
-    ->name('restaurant.search');
